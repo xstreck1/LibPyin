9c9ac7eb5227c7aa85052751108c4680b100dd2a
Adam Streck <adam.streck@gmail.com>
2016-06-13 12:24:02 +0200

    Removed source from root.

--- normal_dist_pdf.h
+++
@@ -1,9 +0,0 @@
-#ifndef NORMAL_DIST_PDF_H
-#define NORMAL_DIST_PDF_H
-
-#include <random>
-
-// given the normal distribution dist computes the probablity of the element val in that distribution
-double normal_dist_pdf(const std::normal_distribution<double> & dist, const double & val);
-
-#endif // NORMAL_DIST_PDF_H
