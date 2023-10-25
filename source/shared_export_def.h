cfbf215d0466d5b27abc13efd1fbd3835b9b5cae
Aymeric Nunge <aymeric.nunge@gmail.com>
2023-10-25 10:47:36 +0200

    statification

--- source/shared_export_def.h
+++ source/shared_export_def.h
@@ -1,14 +1,6 @@
 #ifndef SHARED_EXPORT_DEF_H
 #define SHARED_EXPORT_DEF_H
 
-#ifdef _WIN32
-#ifdef LIBPYIN_BUILD_SHARED
-#define SHARED_EXPORT __declspec(dllexport)
-#else
-#define SHARED_EXPORT __declspec(dllimport)
-#endif
-#else
 #define SHARED_EXPORT
-#endif
 
 #endif // SHARED_EXPORT_DEF_H
